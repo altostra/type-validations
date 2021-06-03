ff773754b2db60c09d141a3d33d5943db6017ae6
Shlomi Borovitz <shlomib@altostra.com>
2021-06-03 17:16:48 +0300

    Add doc-comments

--- src/taggedUnionOf.ts
+++ src/taggedUnionOf.ts
@@ -16,15 +16,35 @@ import { from } from '@reactivex/ix-es2015-cjs/iterable/from'
 import { flatMap } from '@reactivex/ix-es2015-cjs/iterable/operators/flatmap'
 import { map } from '@reactivex/ix-es2015-cjs/iterable/operators/map'
 
+/**
+ * Represents a tagged-union object, which is an object with specific key `TKey`
+ * having a specified `TTags` type
+ *
+ * @param TKey The tag's property name
+ * @param TTags The type of tags that may be used in the `TaggedUnion`
+ */
 export type TaggedUnion<TKey extends string, TTags> = {
   [k in TKey]: TTags
 }
 
+/**
+ * A tagged-union specification using a `Map`
+ * @param TUnion The union type that is defined by the spec
+ * @param TKey The tag's property name
+ * @param TTag The type of tags that may be used in the `TaggedUnion`
+ */
 export type TaggedUnionSpecMap<
   TUnion extends TaggedUnion<TKey, TTag>,
   TKey extends string,
   TTag,
   > = Map<TTag, AnyTypeValidation<TUnion>>
+/**
+ * A tagged-union specification using a simple object.  \
+ * Usable only when all the tags are strings.
+ * @param TUnion The union type that is defined by the spec
+ * @param TKey The tag's property name
+ * @param TTag The type of tags that may be used in the `TaggedUnion`
+ */
 export type TaggedUnionSpecObject<
   TUnion extends TaggedUnion<TKey, TTag>,
   TKey extends string,
@@ -33,6 +53,12 @@ export type TaggedUnionSpecObject<
     [K in TTag]: AnyTypeValidation<TUnion>
   }
 
+/**
+ * Tagged union specification
+ * @param TUnion The union type that is defined by the spec
+ * @param TKey The tag's property name
+ * @param TTag The type of tags that may be used in the `TaggedUnion`
+ */
 export type TaggedUnionSpec<
   TUnion extends TaggedUnion<TKey, TTag>,
   TKey extends string,
@@ -41,7 +67,7 @@ export type TaggedUnionSpec<
   | TaggedUnionSpecMap<TUnion, TKey, TTag>
   | TaggedUnionSpecObject<TUnion, TKey, TTag>
 
-export type MapOrObjectSpec<
+type MapOrObjectSpec<
   TUnion extends TaggedUnion<TKey, TTag>,
   TKey extends string,
   TTag,
@@ -49,25 +75,90 @@ export type MapOrObjectSpec<
   ? TaggedUnionSpec<TUnion, TKey, TTag>
   : TaggedUnionSpecMap<TUnion, TKey, TTag>
 
+/**
+ * Given a `TaggedUnionSpec` type, extracts the union type described by the spec
+ * @param T A `TaggedUnionSpec` type to extract the union from.
+ */
 export type SpecUnion<T> = T extends TaggedUnionSpec<infer TUnion, any, any>
   ? TUnion
   : never
+/**
+ * Given a `TaggedUnionSpec` type, extracts the tags type described by the spec
+ * @param T A `TaggedUnionSpec` type to extract the tag-type from.
+ */
 export type SpecTags<T> = T extends TaggedUnionSpec<any, any, infer TTag>
   ? TTag
   : never
 
+/**
+ * Gets the combined _tagged union_ type from an array type of specifications
+ * @param T A `TaggedUnionSpec[]` array type to get a union of the *tagged-union* types
+ * defined in them
+ */
 export type CombinedUnions<T> = T extends TaggedUnionSpec<any, any, any>[]
   ? (T extends [infer TFirst, ...infer TRest]
     ? SpecUnion<TFirst> | CombinedUnions<TRest>
     : never)
   : never
 
+/**
+ * Gets the combined _tagged union_ **tag** type from an array type of specifications
+ *@param T A `TaggedUnionSpec[]` array type to get a union of the *tagged-union* **tag** types
+ * defined in them
+ */
 export type CombinedTags<T> = T extends TaggedUnionSpec<any, any, any>[]
   ? (T extends [infer TFirst, ...infer TRest]
     ? SpecTags<TFirst> | CombinedTags<TRest>
     : never)
   : never
 
+/**
+ * The type of the `TypeValidation` returned from `taggedUnionOf`.
+ * @param TUnion The union type that is defined by the spec
+ * @param TKey The tag's property name
+ * @param TTag The type of tags that may be used in the `TaggedUnion`
+ */
+export interface TaggedUnionValidation<
+  TUnion extends TaggedUnion<TKey, TTag>,
+  TKey extends string,
+  TTag,
+  > extends TypeValidation<TUnion> {
+  /**
+   * Returns a `TaggedUnionSpecMap` for the validated *tagged-union* types.  \
+   * Can be used in `taggedUnionOf` to compose broader *tagged-union* validations
+   * from existing ones.
+   */
+  unionSpec(): TaggedUnionSpecMap<TUnion, TKey, TTag>
+}
+
+/**
+ * Create `TypeValidation` that validate tagged-unions
+ * @param tagKey The name of the property containing the tag.
+ * @param specs A `TaggedUnionSpec`s that define which validation is used for each specified tag.
+ * @returns A `TaggedUnionValidation` type validation that validates the *tagged-union* type.
+ *
+ * @example
+ * interface A {
+ *   tag: 'a'
+ *   ...
+ * }
+ * interface B {
+ *   tag: 'b'
+ *   ...
+ * }
+ * type TaggedUnion = A | B
+ *
+ * const isA = objectOf<A>({...})
+ * const isB = objectOf<B>({...})
+ *
+ * const isTaggedUnion = taggedUnionOf('tag', {
+ *   // When the tag's values === 'a', use `isA` type validation
+ *   // Due to typescript limitation, the first (and only the first) validation must be casted to the validated type.
+ *   a: isA as TypeValidation<TaggedUnion>,
+ *   // When the tag's values === 'b', use `isB` type validation (this time, no cast is needed)
+ *   b: isB,
+ * })
+ */
 export function taggedUnionOf<
   TKey extends string,
   TSpecs extends NotEmptyArray<TaggedUnionSpec<any, TKey, keyof any>>
@@ -75,6 +166,37 @@ export function taggedUnionOf<
   tagKey: TKey,
   ...specs: TSpecs
 ): TaggedUnionValidation<CombinedUnions<TSpecs>, TKey, CombinedTags<TSpecs>>
+
+/**
+ * Create `TypeValidation` that validate tagged-unions
+ * @param tagKey The name of the property containing the tag.
+ * @param specs A `TaggedUnionSpec`s that define which validation is used for each specified tag.
+ * @returns A `TaggedUnionValidation` type validation that validates the *tagged-union* type.
+ *
+ * @example
+ * const ATag = {}, BTag = {}
+ *
+ * interface A {
+ *   objectTag: object // Assumed to be ATag
+ *   ...
+ * }
+ * interface B {
+ *   objectTag: object // Assumed to be BTag
+ *   ...
+ * }
+ * type TaggedUnion = A | B
+ *
+ * const isA = objectOf<A>({...})
+ * const isB = objectOf<B>({...})
+ *
+ * const isTaggedUnion = taggedUnionOf('objectTag', new Map(
+ *   // When the tag's values === ATag, use `isA` type validation
+ *   // Due to typescript limitation, the first (and only the first) validation must be casted to the validated type.
+ *   [ ATag, isA as TypeValidation<TaggedUnion> ],
+ *   // When the tag's values === BTag, use `isB` type validation (this time, no cast is needed)
+ *   [ BTag, isB ],
+ * ))
+ */
 export function taggedUnionOf<
   TKey extends string,
   TSpecs extends NotEmptyArray<TaggedUnionSpecMap<any, TKey, keyof any>>,
@@ -82,6 +204,35 @@ export function taggedUnionOf<
     tagKey: TKey,
     ...specs: TSpecs
   ): TaggedUnionValidation<CombinedUnions<TSpecs>, TKey, CombinedTags<TSpecs>>
+
+/**
+ * Create `TypeValidation` that validate tagged-unions
+ * @param tagKey The name of the property containing the tag.
+ * @param specs A `TaggedUnionSpec`s that define which validation is used for each specified tag.
+ * @returns A `TaggedUnionValidation` type validation that validates the *tagged-union* type.
+ *
+ * @example
+ * interface A {
+ *   tag: 'a'
+ *   ...
+ * }
+ * interface B {
+ *   tag: 'b'
+ *   ...
+ * }
+ * type TaggedUnion = A | B
+ *
+ * const isA = objectOf<A>({...})
+ * const isB = objectOf<B>({...})
+ *
+ * const isTaggedUnion = taggedUnionOf('tag', {
+ *   // When the tag's values === 'a', use `isA` type validation
+ *   // Due to typescript limitation, the first (and only the first) validation must be casted to the validated type.
+ *   a: isA as TypeValidation<TaggedUnion>,
+ *   // When the tag's values === 'b', use `isB` type validation (this time, no cast is needed)
+ *   b: isB,
+ * })
+ */
 export function taggedUnionOf<
   TUnion extends TaggedUnion<TKey, TTag>,
   TKey extends string,
@@ -90,6 +241,37 @@ export function taggedUnionOf<
     tagKey: TKey,
     ...specs: NotEmptyArray<TaggedUnionSpec<TUnion, TKey, TTag>>
   ): TaggedUnionValidation<TUnion, TKey, TTag>
+
+/**
+ * Create `TypeValidation` that validate tagged-unions
+ * @param tagKey The name of the property containing the tag.
+ * @param specs A `TaggedUnionSpec`s that define which validation is used for each specified tag.
+ * @returns A `TaggedUnionValidation` type validation that validates the *tagged-union* type.
+ *
+ * @example
+ * const ATag = {}, BTag = {}
+ *
+ * interface A {
+ *   objectTag: object // Assumed to be ATag
+ *   ...
+ * }
+ * interface B {
+ *   objectTag: object // Assumed to be BTag
+ *   ...
+ * }
+ * type TaggedUnion = A | B
+ *
+ * const isA = objectOf<A>({...})
+ * const isB = objectOf<B>({...})
+ *
+ * const isTaggedUnion = taggedUnionOf('objectTag', new Map(
+ *   // When the tag's values === ATag, use `isA` type validation
+ *   // Due to typescript limitation, the first (and only the first) validation must be casted to the validated type.
+ *   [ ATag, isA as TypeValidation<TaggedUnion> ],
+ *   // When the tag's values === BTag, use `isB` type validation (this time, no cast is needed)
+ *   [ BTag, isB ],
+ * ))
+ */
 export function taggedUnionOf<
   TUnion extends TaggedUnion<TKey, TTag>,
   TKey extends string,
@@ -175,12 +357,4 @@ function specAsMap<
         map(([tag, validation]) => [tag, asRejectingValidator(validation)])
       )
   }
-}
-
-export interface TaggedUnionValidation<
-  TUnion extends TaggedUnion<TKey, TTag>,
-  TKey extends string,
-  TTag,
-  > extends TypeValidation<TUnion> {
-  unionSpec(): TaggedUnionSpecMap<TUnion, TKey, TTag>
 }
