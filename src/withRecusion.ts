c33059c1fd8feae543f557f1f07fa5c178366650
Shlomi Borovitz <shlomib@altostra.com>
2022-04-03 10:29:37 +0300

    Removed "lcoal" depths (and renamed globals to prev local names)

--- src/withRecusion.ts
+++ src/withRecusion.ts
@@ -5,32 +5,18 @@ import { AnyTypeValidation, typeValidatorType } from '.'
 export interface WithRecursionOptionsBase {
   maxDepth?: number
   skipDepth?: number
-  maxGlobalDepth?: number
-  skipGlobalDepth?: number
 }
 export interface WithRecursionMaxOptions extends WithRecursionOptionsBase {
   // The maximum amount of times the recursion is allowed to run before rejecting the data
   maxDepth?: number
   // The maximum amount of times the recursion is allowed to run before accepting the data
   skipDepth?: never
-  // The maximum amount of times any recursion is allowed to run before rejecting the data
-  // Should be used when you have recursion between more than one type
-  maxGlobalDepth?: number
-  // The maximum amount of times any recursion is allowed to run before accepting the data
-  // Should be used when you have recursion between more than one type
-  skipGlobalDepth?: never
 }
 export interface WithRecursionSkipOptions extends WithRecursionOptionsBase {
   // The maximum amount of times the recursion is allowed to run before rejecting the data
   maxDepth?: never
   // The maximum amount of times the recursion is allowed to run before accepting the data
   skipDepth?: number
-  // The maximum amount of times any recursion is allowed to run before rejecting the data
-  // Should be used when you have recursion between more than one type
-  maxGlobalDepth?: never
-  // The maximum amount of times any recursion is allowed to run before accepting the data
-  // Should be used when you have recursion between more than one type
-  skipGlobalDepth?: number
 }
 
 /**
@@ -59,35 +45,27 @@ export function withRecursion<T>(
 ): TypeValidation<T> {
   const {
     maxDepth,
-    maxGlobalDepth,
     skipDepth,
-    skipGlobalDepth,
   } = validateOptions(options)
 
-  let currentDepth = 0
   const hasGlobal = typeof currentGlobalDepth === 'number'
-  currentGlobalDepth = maxGlobalDepth || skipGlobalDepth
-    ? currentGlobalDepth ?? 0
-    : currentGlobalDepth
+  currentGlobalDepth ??= 0
 
   let result: TypeValidation<T>
   const type = () => `↻(${result[typeValidatorType]})`
   const resultReference = registerRejectingValidator(
     (value: unknown, reject): value is T => {
+      if (result === undefined) {
+        throw new Error('Recursive validation reference must not be called before initialization')
+      }
+
       try {
-        currentDepth++
         incGlobal()
 
-        if (
-          (maxDepth && currentDepth > maxDepth) ||
-          (maxGlobalDepth && currentGlobalDepth! > maxGlobalDepth)
-        ) {
+        if (maxDepth && currentGlobalDepth! > maxDepth) {
           reject?.(createRejection('Recursion max depth has reached', type()))
         }
-        else if (
-          (skipDepth && currentDepth > skipDepth) ||
-          (skipGlobalDepth && currentGlobalDepth! > skipGlobalDepth)
-        ) {
+        else if (skipDepth && currentGlobalDepth! > skipDepth) {
           // Skip farther validations
           return true
         }
@@ -96,8 +74,6 @@ export function withRecursion<T>(
       }
       finally {
         decGlobal()
-        // Decrease until zero
-        currentDepth = currentDepth && (currentDepth - 1)
 
         if (currentGlobalDepth === 0 && !hasGlobal) {
           currentGlobalDepth = undefined
@@ -115,9 +91,7 @@ export function withRecursion<T>(
 
 const optionsKeys: (keyof WithRecursionOptions)[] = [
   'maxDepth',
-  'maxGlobalDepth',
   'skipDepth',
-  'skipGlobalDepth',
 ]
 function validateOptions(options: WithRecursionOptions = {}): WithRecursionOptions {
   // All must be positive integers
@@ -140,26 +114,6 @@ function validateOptions(options: WithRecursionOptions = {}): WithRecursionOptio
   // Skip and max cannot be used in the same time
   if (options.maxDepth !== undefined && options.skipDepth !== undefined) {
     throw new Error('`withRecursion` maxDepth at same time with skipDepth')
-  }
-  if (options.maxGlobalDepth !== undefined && options.skipGlobalDepth !== undefined) {
-    throw new Error('`withRecursion` maxGlobalDepth at same time with skipGlobalDepth')
-  }
-
-  // Global must be greater than (or equals) to type-local
-  if (
-    options.maxDepth !== undefined &&
-    options.maxGlobalDepth !== undefined &&
-    options.maxDepth > options.maxGlobalDepth
-  ) {
-    throw new Error('`withRecursion` maxDepth must be lesser than maxGlobalDepth')
-  }
-
-  if (
-    options.skipDepth !== undefined &&
-    options.skipGlobalDepth !== undefined &&
-    options.skipDepth > options.skipGlobalDepth
-  ) {
-    throw new Error('`withRecursion` skipDepth must be lesser than skipGlobalDepth')
   }
 
   return options
